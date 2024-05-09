b1a87b86dd784ca83bbb3490105e252aceb3b902
Ian Ekwoge <52867919+vizallati@users.noreply.github.com>
2024-05-09 20:39:47 +0100

    Update test_cases.py

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -9,21 +9,3 @@ from steps.then import *
 @scenario('features/qa_test.feature', 'Test entire form (valid details)')
 def test_qa_form_valid_details():
     pass
-
-
-# @pytest.mark.case_id('QA-002')
-# @scenario('features/qa_test.feature', 'Check crawl results')
-# def test_check_crawl_results():
-#     pass
-#
-#
-# @pytest.mark.case_id('WP-003')
-# @scenario('features/qa_test.feature', 'Deletion of previous crawl results')
-# def test_check_crawl_results_deletion():
-#     pass
-#
-#
-# @pytest.mark.case_id('WP-004')
-# @scenario('features/qa_test.feature', 'Check deletion of sitemap file after crawl')
-# def test_check_sitemap_deletion():
-#     pass
